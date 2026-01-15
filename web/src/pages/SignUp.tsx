5cdc4adea3aca0f43c49ee1d6621cc5105a962c3
João Lucas <ylucasslalves@gmail.com>
2026-01-15 19:46:14 -0300

    Criando SignUp

--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -4,24 +4,30 @@ import Input from "../components/Input"
 import { useState } from "react"
 
 const SigIn = () => {
+  const [name, setName] = useState<string>("")
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
+  const [passwordConfirm, setPasswordConfirm] = useState<string>("")
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault()
-    console.log(email, password)
+    console.log(name, email, password, passwordConfirm)
 
     setEmail("")
     setPassword("")
+    setName("")
+    setPasswordConfirm("")
   }
 
   return (
     <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
+      <Input required legend="Name" value={name} placeholder="Seu nome" onChange={(e) => setName(e.target.value)}/>
       <Input type="email" required legend="E-mail" value={email} placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)}/>
       <Input type="password" required legend="Senha" value={password} placeholder="123456" onChange={(e) => setPassword(e.target.value)}/>
-      <Button type="submit">Entrar</Button>
-      <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Criar contar</a>
+      <Input type="password" required legend="Confirmação da Senha" value={password} placeholder="123456" onChange={(e) => setPasswordConfirm(e.target.value)}/>
+      <Button type="submit" isLoading={isLoading}>Cadastrar</Button>
+      <a href="/" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Já tenho uma conta</a>
     </form>
   )
 }
--- web/src/routes/AuthRoutes.tsx
+++ web/src/routes/AuthRoutes.tsx
@@ -1,6 +1,8 @@
 import {Routes, Route} from "react-router"
 
 import SigIn from "../pages/SigIn"
+import SignUp from "../pages/SignUp"
+
 import AuthLayout from "../components/AuthLayout"
 
 const AuthRoutes = () => {
@@ -8,6 +10,7 @@ const AuthRoutes = () => {
     <Routes>
       <Route path="/" element={<AuthLayout />}>
         <Route path="/" element={<SigIn />}/>
+        <Route path="/signup" element={<SignUp />}/>
       </Route>
     </Routes>
   )
