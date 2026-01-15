bafe15c430dc760225c624d6bcc3cd5ef65edf28
João Lucas <ylucasslalves@gmail.com>
2026-01-15 14:27:22 -0300

    criando onSubmit com states

--- web/src/pages/SigIn.tsx
+++ web/src/pages/SigIn.tsx
@@ -1,12 +1,26 @@
 import Button from "../components/Button"
 import Input from "../components/Input"
 
+import { useState } from "react"
+
 const SigIn = () => {
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [isLoaidng, setIsLoading] = useState<boolean>(false)
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    console.log(email, password)
+
+    setEmail("")
+    setPassword("")
+  }
+
   return (
-    <form onSubmit={((e) => e.preventDefault())} className="w-full flex flex-col gap-4">
-      <Input type="email" required legend="E-mail" placeholder="seu@email.com"/>
-      <Input type="password" required legend="Senha" placeholder="123456"/>
-      <Button>Entrar</Button>
+    <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
+      <Input type="email" required legend="E-mail" value={email} placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)}/>
+      <Input type="password" required legend="Senha" value={password} placeholder="123456" onChange={(e) => setPassword(e.target.value)}/>
+      <Button type="submit">Entrar</Button>
     </form>
   )
 }
