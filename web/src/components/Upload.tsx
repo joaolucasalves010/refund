da7a3e26ad70ed67311623d28f60913c8aefafc8
João Lucas <ylucasslalves@gmail.com>
2026-01-20 12:15:28 -0300

    Botão de enviar

--- web/src/components/Upload.tsx
+++ web/src/components/Upload.tsx
@@ -9,7 +9,7 @@ const Upload = ({filename = null, ...rest}: Props) => {
     <div>
       <legend className="uppercase text-xxs text-gray-200 mb-2">Comprovante</legend>
       <div className="w-full h-12 flex items-center rounded-lg border border-gray-300 text-sm text-gray-100 bg-transparent outline-none">
-        <input type="file" id="upload" className="hidden" />
+        <input type="file" id="upload" className="hidden" {...rest} />
         <span className="text-xs text-gray-100 flex-1 pl-4">
           {filename ?? "Selecione o arquivo"}
         </span>
--- web/src/pages/Refund.tsx
+++ web/src/pages/Refund.tsx
@@ -2,14 +2,28 @@ import { useState } from "react"
 import Input from "../components/Input"
 import Select from "../components/Select"
 import Upload from "../components/Upload"
+import Button from "../components/Button"
 import { CATEGORIES, CATEGORIES_KEYS } from "../utlis/categories"
 
 const Refund = () => {
-  const [category, setCategory] = useState("")
+  const [name, setName] = useState<string>("")
+  const [category, setCategory] = useState<string>("")
+  const [amount, setAmount] = useState<string>("")
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [filename, setFilename] = useState<File | null>(null)
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    console.log(`Categoria: ${category}`)
+    console.log(`Nome: ${name}`)
+    console.log(`Quantia: ${amount}`)
+    console.log(`Nome do arquivo: ${filename?.name}`)
+  }
 
 
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]:">
+    <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]:">
       <header>
         <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso.</p>
@@ -18,6 +32,8 @@ const Refund = () => {
       <Input 
         required
         legend="Nome da solicitação"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
       />
       <div className="flex gap-4">
         <Select legend="Categoria" onChange={(e) => setCategory(e.target.value)}>
@@ -27,10 +43,12 @@ const Refund = () => {
             </option>
           ))}
         </Select>
-        <Input legend="Valor" required/>
+        <Input legend="Valor" required onChange={(e) => setAmount(e.target.value)} type="number" value={amount}/>
       </div>
 
-      <Upload filename="teste.png"/>
+      <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])}/>
+      
+      <Button type="submit" isLoading={isLoading}>Enviar</Button>
     </form>
   )
 }
